43dc4760dafc68d8d1e9c04640aa2b8495d14bb5
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2026-03-13 23:17:06 +0900

    Make use of pbio_imu_set_base_orientation()

--- drivers/spike/hub/imu.c
+++ drivers/spike/hub/imu.c
@@ -12,12 +12,6 @@
 #include <pbio/imu.h>
 #include <math.h>
 
-static pbio_geometry_matrix_3x3_t rot_matrix = {
-  .m11 = 1.0f, .m12 = 0.0f, .m13 = 0.0f,
-  .m21 = 0.0f, .m22 = 1.0f, .m23 = 0.0f,
-  .m31 = 0.0f, .m32 = 0.0f, .m33 = 1.0f,
-};
-
 /*
  * We implement the IMU API by just wrapping functions in external/libpybricks/lib/pbio/src/imu.c.
  */
@@ -45,21 +39,17 @@ void hub_imu_set_tilt(float angle) {
   double T = (double) angle * M_PI / 180.0f;
   double sinT = sin(T);
   double cosT = cos(T);
-  rot_matrix.m11 = +cosT; rot_matrix.m12 = 0.0f; rot_matrix.m13 = -sinT;
-  rot_matrix.m21 = 0.0f ; rot_matrix.m22 = 1.0f; rot_matrix.m23 = 0.0f ;
-  rot_matrix.m31 = +sinT; rot_matrix.m32 = 0.0f; rot_matrix.m33 = +cosT;
+  pbio_geometry_xyz_t front = { .x = +cosT, .y = 0.0f, .z = +sinT };
+  pbio_geometry_xyz_t top   = { .x = -sinT, .y = 0.0f, .z = +cosT };
+  pbio_imu_set_base_orientation(&front, &top);
 }
 
 void hub_imu_get_acceleration(float accel[3]) {
-  pbio_geometry_xyz_t values;
-  pbio_imu_get_acceleration(&values, true);
-  pbio_geometry_vector_map(&rot_matrix, &values, (pbio_geometry_xyz_t *) accel);
+  pbio_imu_get_acceleration((pbio_geometry_xyz_t *) accel, true);
 }
 
 void hub_imu_get_angular_velocity(float angv[3]) {
-  pbio_geometry_xyz_t values;
-  pbio_imu_get_angular_velocity(&values, true);
-  pbio_geometry_vector_map(&rot_matrix, &values, (pbio_geometry_xyz_t *) angv);
+  pbio_imu_get_angular_velocity((pbio_geometry_xyz_t *) angv, true);
 }
 
 float hub_imu_get_temperature(void) {
